de11c08d44a7a752abc0a24671dcecf18d7264b9
Daniel Fox <13392079@students.lincoln.ac.uk>
2016-01-30 14:39:58 +0000

    added extra UI and Mouse clicks

---
+++ GGJ Right to Live/Assets/TimeAndAge.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeAndAge : MonoBehaviour {
+
+    public int attemptCount = 0;
+    public GameObject UITimeAndAge;
+    public Text Time;
+    Text GameOver;
+    // Use this for initialization
+    void Start()
+    {
+        UITimeAndAge = gameObject;
+        Time = UITimeAndAge.GetComponent<Text>();
+        Time.text = "Testing " + attemptCount;
+        Time.fontSize = 30;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Time.text = "Testing " + attemptCount;
+        Time.fontSize = 30;
+    }
+}
--- GGJ Right to Live/Assets/WhichBall.cs
+++ GGJ Right to Live/Assets/WhichBall.cs
@@ -1,32 +1,131 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WhichBall : MonoBehaviour {
-
+public class WhichBall : MonoBehaviour
+{
     public int x;
+    public bool cupMove1 = false;
+    public bool cupMove2 = false;
+    public bool blackRight = false;
+    public bool blackLeft = false;
     public Vector3 Right = new Vector3(0.5f, 0.19f, 0);
     public Vector3 Left = new Vector3(-0.5f, 0.19f, 0);
     public Transform whiteBall;
     public Transform blackBall;
+    public GameObject cupOne;
+    public GameObject cupTwo;
+    public GameObject textScript;
+    public TimeAndAge timeScript;
 
-	// Use this for initialization
-	void Start ()
+    // ============================
+    // Use this for initialization
+    // ============================
+    void Start()
     {
+        // ================
+        // Finds the Cups
+        // ===============
+        cupOne = GameObject.FindWithTag("cupOne");
+        cupTwo = GameObject.FindWithTag("cupTwo");
+        textScript = GameObject.FindWithTag("TextCreator");
+
+        // ==============================================================================
+        // Makes the ball spawn randomly between two points at the beginning of the game
+        // ==============================================================================
         x = Random.Range(0, 11);
         if (x <= 5)
         {
             Instantiate(whiteBall, Right, Quaternion.identity);
             Instantiate(blackBall, Left, Quaternion.identity);
+            blackLeft = true;
+            blackRight = false;
         }
-        else if (x > 5) {
+        else if (x > 5)
+        {
             Instantiate(whiteBall, Left, Quaternion.identity);
             Instantiate(blackBall, Right, Quaternion.identity);
+            blackRight = true;
+            blackLeft = false;
         }
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // ================================
+    // Update is called once per frame
+    // ================================
+    void Update()
     {
-	    
-	}
+        // =================
+        // Clicking objects
+        // =================
+        if (Input.GetMouseButtonDown(0))
+        {
+            // =======================================
+            // Creates a ray for clicking the objects
+            // =======================================
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+            print("Mouse clicked");
+
+            // =======================
+            // What does the ray hit?
+            // =======================
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.collider.tag == "cupOne")
+                {
+                    cupMove1 = true;
+                    print("Hit Something");
+                }
+                if (hit.collider.tag == "cupTwo")
+                {
+                    cupMove2 = true;
+                    print("Hit Something");
+                }
+            }
+        }
+
+        // ==============================
+        // Translate the cups if clicked
+        // ==============================
+        if (cupMove1 == true)
+        {
+            // Cup on the Right
+            cupOne.transform.position = Vector3.MoveTowards(cupOne.transform.position, new Vector3(0.5f, 0.6f, 0.1f), 0.25f * Time.deltaTime);
+            cupTwo.transform.position = Vector3.MoveTowards(cupTwo.transform.position, new Vector3(-0.5f, 0.6f, 0.1f), 0.25f * Time.deltaTime);
+            if (blackRight == true)
+            {
+                print("Game Over");
+            }
+            else
+            {
+                print("Continue");
+                timeScript = textScript.GetComponent<TimeAndAge>();
+                timeScript.attemptCount++;
+            }
+        }
+        if (cupMove2 == true)
+        {
+            // Cup on the Left
+            cupOne.transform.position = Vector3.MoveTowards(cupOne.transform.position, new Vector3(0.5f, 0.6f, 0.1f), 0.25f * Time.deltaTime);
+            cupTwo.transform.position = Vector3.MoveTowards(cupTwo.transform.position, new Vector3(-0.5f, 0.6f, 0.1f), 0.25f * Time.deltaTime);
+            if (blackLeft == true)
+            {
+                print("Game Over");
+            }
+            else
+            {
+                print("Continue");
+                timeScript = textScript.GetComponent<TimeAndAge>();
+                timeScript.attemptCount++;
+            }
+        }
+
+        // ============
+        // Live of Die
+        // ============
+
+
+
+    }
 }
